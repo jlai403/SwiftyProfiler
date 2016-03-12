388552ef5ac41c769d6afdc6e458d7f6ae4e7961
Joey <j.lai@live.ca>
2016-03-12 00:22:17 -0700

    Remove build folder

--- build/SwiftyProfiler.build/Release-iphoneos/SwiftyProfiler.build/DerivedSources/SwiftyProfiler_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char SwiftyProfilerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftyProfiler  PROJECT:SwiftyProfiler-1" "\n"; const double SwiftyProfilerVersionNumber __attribute__ ((used)) = (double)1.;
--- build/SwiftyProfiler.build/Release-iphoneos/SwiftyProfiler.build/Objects-normal/arm64/SwiftyProfiler_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jlai/Developer/Xcode/SwiftyProfiler/build/SwiftyProfiler.build/Release-iphoneos/SwiftyProfiler.build/DerivedSources/SwiftyProfiler_vers.c
