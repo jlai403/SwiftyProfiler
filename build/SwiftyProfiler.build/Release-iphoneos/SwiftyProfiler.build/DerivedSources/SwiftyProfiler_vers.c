039992f037c93c37e4d0bd46fbd3e8030691d677
Joey <j.lai@live.ca>
2016-03-12 00:17:32 -0700

    Include code coverage

---
+++ build/SwiftyProfiler.build/Release-iphoneos/SwiftyProfiler.build/DerivedSources/SwiftyProfiler_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char SwiftyProfilerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftyProfiler  PROJECT:SwiftyProfiler-1" "\n"; const double SwiftyProfilerVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/SwiftyProfiler.build/Release-iphoneos/SwiftyProfiler.build/Objects-normal/arm64/SwiftyProfiler_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jlai/Developer/Xcode/SwiftyProfiler/build/SwiftyProfiler.build/Release-iphoneos/SwiftyProfiler.build/DerivedSources/SwiftyProfiler_vers.c
